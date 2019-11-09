7cfe7f8c8df86540885805dabf678e5b6f87d20a
Sindre Sorhus <sindresorhus@gmail.com>
2019-11-09 12:45:13 +0700

    Simplify

--- Sources/screen-capture-permissions/ScreenCapturePermissions.swift
+++ Sources/screen-capture-permissions/ScreenCapturePermissions.swift
@@ -2,18 +2,16 @@ import Cocoa
 
 struct ScreenCapturePermissions {
   static func checkPermissions() -> String {
-    guard let stream = CGDisplayStream(
+    let stream = CGDisplayStream(
       dispatchQueueDisplay: CGMainDisplayID(),
       outputWidth: 1,
       outputHeight: 1,
       pixelFormat: Int32(kCVPixelFormatType_32BGRA),
       properties: nil,
       queue: DispatchQueue.global(),
-      handler: { status, displayTime, frameSurface, updateRef in })
-    else {
-      return "false";
-    }
+      handler: nil
+    )
 
-    return "true";
+    return stream == nil ? "false" : "true"
   }
 }
